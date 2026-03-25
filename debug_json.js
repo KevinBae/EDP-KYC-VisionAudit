8e13cd8822372fa240bffdf4fc1382c60266b3dc
Kevin Bae <kevinybae@gmail.com>
2026-03-24 19:15:07 -0500

    Update debug_json.js
    render

--- debug_json.js
+++ debug_json.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 (async () => {
     try {
-        const res = await axios.post('http://localhost:3000/api/audit', { url: 'https://drinkag1.com' });
+        const res = await axios.post('https://edp-kyc-visionaudit.onrender.com', { url: 'https://drinkag1.com' });
         console.log(JSON.stringify(res.data, null, 2));
     } catch (e) {
         console.error(e.message);
